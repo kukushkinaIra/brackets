07aa0fed9f80170f0dc0d8996e08fc9770317aad
kukushkinaIra <55156673+kukushkinaIra@users.noreply.github.com>
2020-10-24 20:23:19 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(str);
+ while (/\(\)/.test(str) || /\[\]/.test(str) || /\{\}/.test(str) || /\|\|/.test(str) || /12/.test(str) || 
+ /34/.test(str) || /56/.test(str) || /77/.test(str) || /88/.test(str)) {
+   str=str.replace('()', '');
+   str=str.replace('[]', '');
+   str=str.replace('{}', '');
+   str=str.replace('||', '');
+   str=str.replace('12', '');
+   str=str.replace('34', '');
+   str=str.replace('56', '');
+   str=str.replace('77', '');
+   str=str.replace('88', '');
+ }
+ if (str == '')
+ return true;
+ else return false;
 }
